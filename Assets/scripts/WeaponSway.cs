ee1f3603aa0e1b0b0b20701d086f9c04d9060eef
gangstapetergriffin <144125776+gangstapetergriffin@users.noreply.github.com>
2023-11-29 12:40:56 +0100

    add cool swey

--- Assets/scripts/WeaponSway.cs
+++ Assets/scripts/WeaponSway.cs
@@ -7,7 +7,7 @@ public class testsway : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     [Header("Sway Settings")]
@@ -16,28 +16,35 @@ public class testsway : MonoBehaviour
 
     [SerializeField] private float swayMultiplier;
 
-    float swayCounter = 0f;
-    float swayMagnitude = .01f;
+    float bobbingCounter = 0f;
+    float bobbingMagnitude = .01f;
 
     // Update is called once per frame
     void Update()
     {
+
         // static hand movement for the gun model
-        float addAmount = Mathf.Sin(0.01f * swayCounter) * swayMagnitude * Time.deltaTime;
+        float addAmount = Mathf.Sin(0.01f * bobbingCounter) * bobbingMagnitude * Time.deltaTime;
         transform.localPosition += Vector3.up * addAmount;
-        swayCounter++;
+        bobbingCounter++;
+
+
 
         // we want to have the mouse inpoot :3
         float mouseX = Input.GetAxisRaw("Mouse X") * swayMultiplier;
 
         float mouseY = Input.GetAxisRaw("Mouse Y") * swayMultiplier;
 
+        float mouseZ = Input.GetAxisRaw("Mouse X") * swayMultiplier * 0.5f;
+
         // calculate the target rotation
-        Quaternion rotationX = Quaternion.AngleAxis(mouseY, Vector3.left);
+        Quaternion rotationX = Quaternion.AngleAxis(-mouseY, Vector3.left);
         Quaternion rotationY = Quaternion.AngleAxis(mouseX, Vector3.up);
+        Quaternion rotationZ = Quaternion.AngleAxis(mouseZ, Vector3.forward);
 
 
-        Quaternion targetRotation = rotationX * rotationY;
+        Quaternion targetRotation = rotationX * rotationY * rotationZ;
+
 
         // finally rotate the weapon
         transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation, smooth * Time.deltaTime);
