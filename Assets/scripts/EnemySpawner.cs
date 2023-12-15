947dd06d0878edf3a89d8c98283ad48fd3079877
gangstapetergriffin <144125776+gangstapetergriffin@users.noreply.github.com>
2023-12-15 23:23:16 +0100

    final commit before turning in

--- Assets/scripts/EnemySpawner.cs
+++ Assets/scripts/EnemySpawner.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class EnemySpawner : MonoBehaviour
 {
     public GameObject OGEnemy;
-    public int EnemyMax = 400;
+    public float EnemyMax = 400;
+    public TMP_Text enemAmount;
+    public TMP_Text Wave;
+    public GameObject[] Spawnpoints;
+    public Vector3[] Positions;
+    private int waves = 1;
+    private int enemiesAlive = 0;
+    private bool wavestart;
 
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < EnemyMax; i++)
+        Invoke("spawnZombies", 10);
+    }
+
+    void spawnZombies()
+    {
+        wavestart = true;
+        Positions = new Vector3[Spawnpoints.Length];
+        for (int i = 0; i < Spawnpoints.Length; i++)
         {
-            GameObject enemy = Instantiate(OGEnemy);
+            Positions[i] = Spawnpoints[i].transform.position;
+        }
+        for (int b = 0; b < EnemyMax; b++)
+        {
+            int randomIndex = Random.Range(0, Spawnpoints.Length);
+            GameObject enemy = Instantiate(OGEnemy, Positions[randomIndex], Quaternion.identity);
+        }
+    }
+
+    private void Update()
+    {
+        int enemiesAlive = GameObject.FindGameObjectsWithTag("NPC").Length;
+
+        enemAmount.text = "Enemies Alive: " + enemiesAlive.ToString();
+        if (enemiesAlive == 0 && (wavestart == true))
+        {
+            wavestart = false;
+            waves++;
+            EnemyMax = EnemyMax * 1.5f;
+            Invoke("spawnZombies",3);
+            Wave.text = "Wave: " + waves;
+
         }
     }
 
